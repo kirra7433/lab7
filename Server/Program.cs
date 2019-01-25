adc385a35b57a76882f8ece4112549982ac58be7
Marian <16372420+kirra7433@users.noreply.github.com>
2019-01-25 23:05:31 +0500

    Initial commit

---
+++ Server/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server
+{
+    class Program
+    {
+        public static TcpListener listener;
+        public static ECDiffieHellmanCng serverDH;
+
+        public static void GenerationDHKey()
+        {
+            serverDH = new ECDiffieHellmanCng();
+            serverDH.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            serverDH.HashAlgorithm = CngAlgorithm.Sha256;
+            byte[] serverPublicKey = serverDH.PublicKey.ToByteArray();
+            File.WriteAllBytes(@"D:\OSU\ЗПиД\ЗПиД7\Публичный ключ сервера.txt", serverPublicKey);
+        }
+
+        public static void GetMessages()
+        {
+            TcpClient client = listener.AcceptTcpClient();
+            byte[] encryptedMessage = new byte[1024];
+            NetworkStream nstream = client.GetStream();
+            int count = nstream.Read(encryptedMessage, 0, encryptedMessage.Length);
+            byte[] message = new byte[count];
+            Array.Copy(encryptedMessage, message, count);
+            byte[] decryptedMessage = Decrypt(message);
+            string result = Encoding.UTF8.GetString(decryptedMessage);
+            Console.WriteLine("<<" + result);
+        }
+
+        public static byte[] Decrypt(byte[] requestBytes)
+        {
+            byte[] publicKeyClient =
+               File.ReadAllBytes(@"D:\OSU\ЗПиД\ЗПиД7\Публичный ключ клиента.txt");
+            byte[] commonKeyForServer =
+               serverDH.DeriveKeyMaterial(CngKey.Import(publicKeyClient, CngKeyBlobFormat.EccPublicBlob));
+            Aes aesServer = new AesCryptoServiceProvider();
+            aesServer.Key = commonKeyForServer;
+            aesServer.IV = new byte[16];
+            aesServer.Padding = PaddingMode.None;
+            byte[] DecryptToServer;
+            int rc = 0;
+            using (MemoryStream ms = new MemoryStream(requestBytes))
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aesServer.CreateDecryptor(), CryptoStreamMode.Read))
+                {
+                    DecryptToServer = new byte[ms.Length];
+                    rc = cs.Read(DecryptToServer, 0, DecryptToServer.Length);
+                    cs.Close();
+                }
+                ms.Close();
+            }
+            return DecryptToServer;
+        }
+
+        private static void Main(string[] args)
+        {
+            IPAddress ipAddress = IPAddress.Parse("127.0.0.1");
+            listener = new TcpListener(ipAddress, 7777);
+            listener.Start();
+            while (true)
+            {
+                GenerationDHKey();
+                GetMessages();
+            }
+            listener.Stop();
+        }
+    }
+}
---
+++ ЗПиД7/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ЗПиД7
+{
+    internal class Program
+    {
+        public static TcpClient client;
+        public static ECDiffieHellmanCng clientDH;
+
+        public static void GenerationDHKey()
+        {
+            clientDH = new ECDiffieHellmanCng();
+            clientDH.KeyDerivationFunction = ECDiffieHellmanKeyDerivationFunction.Hash;
+            clientDH.HashAlgorithm = CngAlgorithm.Sha256;
+            byte[] clientPublicKey = clientDH.PublicKey.ToByteArray();
+            File.WriteAllBytes(@"D:\OSU\ЗПиД\ЗПиД7\Публичный ключ клиента.txt", clientPublicKey);
+        }
+        public static void WriteAndSendMessage(string message)
+        {
+            byte[] messageBytes = Encoding.UTF8.GetBytes(message);
+            byte[] enctyptedMessage = Encrypt(messageBytes);
+            NetworkStream nstream = client.GetStream();
+            nstream.Write(enctyptedMessage, 0, enctyptedMessage.Length);
+            nstream.Close();
+            client.Close();
+        }
+
+        public static byte[] Encrypt(byte[] messageBytes)
+        {
+            byte[] publicKeyServer =
+                     File.ReadAllBytes(@"D:\OSU\ЗПиД\ЗПиД7\Публичный ключ сервера.txt");
+            byte[] commonKeyForClient =
+                clientDH.DeriveKeyMaterial(CngKey.Import(publicKeyServer, CngKeyBlobFormat.EccPublicBlob));
+            Aes aesClient = new AesCryptoServiceProvider();
+            aesClient.Key = commonKeyForClient;
+            aesClient.IV = new byte[16];
+            aesClient.Padding = PaddingMode.Zeros;
+            byte[] EncryptFromClient;
+            using (MemoryStream ms = new MemoryStream())
+            {
+                using (CryptoStream cs = new CryptoStream(ms, aesClient.CreateEncryptor(), CryptoStreamMode.Write))
+                {
+                    cs.Write(messageBytes, 0, messageBytes.Length);
+                    cs.Close();
+                }
+                ms.Close();
+                EncryptFromClient = ms.ToArray();
+            }
+            return EncryptFromClient;
+        }
+        private static void Main(string[] args)
+        {
+            while (true)
+            {
+                GenerationDHKey();
+                client = new TcpClient("127.0.0.1", 7777);
+                Console.Write(">>");
+                string message = Console.ReadLine();
+                WriteAndSendMessage(message);
+
+            }
+        }
+    }
+}
+
+
